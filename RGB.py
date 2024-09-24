4994a1992bcb126d86616adc2b0a458d932c15c9
Olly Evans <136471380+olly-evans@users.noreply.github.com>
2024-09-24 21:07:55 +0100

    plt.hist and labels etc.

--- RGB.py
+++ RGB.py
@@ -8,3 +8,11 @@ assert image is not None, "file could not be read, check with os.path.exists()"
 # Stupid method just finds the frequency values :(
 hist = cv2.calcHist([image], [2], None, [256], [0, 256])
 print(hist)
+
+plt.hist(hist, bins=256, range=None, density=False, weights=None, cumulative=False, color=None, label=None, alpha=1.0, edgecolor='black')
+
+plt.title('Red Color Channel Histogram')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+plt.show()
